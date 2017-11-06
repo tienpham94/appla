7d0d9ebc346714b8c4a40a321a32ad8b741708d2
Olli Alm <olli.alm@metropolia.fi>
2017-11-06 22:15:01 +0200

    new master with correct folder structure

--- app.js
+++ app.js
@@ -1,52 +1,33 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var expressValidator = require('express-validator');
-var path = require('path');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var Constants = require('./config/constants');
-var app = express();
-
-var router = require('./router');
-// var socket = require('./socket');
-var ioServer 	= require('./socket')(app);
-
-// var http = require('http').Server(app);
-var port = process.env.PORT || 3000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(expressValidator()); // this line must be immediately after express.bodyParser()!
-
-app.use('/api/', router);
-
-app.use('/libs', express.static(__dirname + '/public/libs'));
-app.use('/bower_components', express.static(__dirname + '/public/bower_components'));
-app.use('/css', express.static(__dirname + '/public/css'));
-app.use('/img', express.static(__dirname + '/public/img'));
-app.use('/modules', express.static(__dirname + '/public/modules'));
-app.use('/templates', express.static(__dirname + '/public/templates'));
-
-app.all('/*', function(req, res, next) {
-    // Just send the index.html for other files to support HTML5Mode
-    res.sendFile('index.html', { root: __dirname + '/public' });
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
+var connection_count = 0
 
-// Handle 404 error.
-// The last middleware.
-app.use("*", function (req, res) {
-    res.status(404).send('404');
-});
+io.on('connection', function(socket){
+  connection_count += 1
+  console.log('user connected');
+  console.log('Connections: ' + connection_count);
 
-// socket.connect(http);
-//
-// http.listen(3000, function () {
-//     var host = http.address().address;
-//     var port = http.address().port;
-//     console.log("Server listening at http://%s:%s", host, port);
-// });
+  socket.on('disconnect', function(){
+    connection_count -= 1
+    console.log('user disconnected');
+    console.log('Connections: ' + connection_count);
+  });
 
-ioServer.listen(port);
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+  });
 
-// module.exports = app;
+})
 
+
+http.listen(3000, function(){
+  var host = http.address().address
+  var port = http.address().port
+  console.log("Server listening at http://%s:%s", host, port);
+})
