86bc098860d49b1ed246e28ab9c06a84334506f9
Tien Pham <r3dsky02@yahoo.com.vn>
2017-11-27 14:59:33 +0200

    Wordcloud added to AngularJS template

--- public/modules/room/room.directive.js
+++ public/modules/room/room.directive.js
@@ -3,13 +3,15 @@ angular.module('room.module')
         // template: "<div class=''><h1 class='new-class'>{{ title }}</h1><button ng-click='someClickTest()'>Click me!</button></div>",
         templateUrl: '/modules/room/views/wordcloud.html',
         controller: function($scope){
-            $scope.title = 'Wordcloud'
+            // $scope.title = 'Wordcloud'
             // $scope.clicks = 0
-            $scope.someClickTest = function(){
-                console.log("clicked")
-                $scope.clicks += 1
-                $scope.title = 'Clicked ' + $scope.clicks + ' times'
-            }
+            // $scope.someClickTest = function(){
+            //     console.log("clicked")
+            //     $scope.clicks += 1
+            //     $scope.title = 'Clicked ' + $scope.clicks + ' times'
+            // }
+          $scope.drawWordCloud = drawWordCloud;
+
         }
     })
     .directive('userList', ['roomSocket','RoomService', function (roomSocket, RoomService) {
@@ -60,3 +62,59 @@ angular.module('room.module')
             }
         }
     }]);
+
+var stopWords = ["a", "an", "and", "tien"];
+
+function wordFreq(string) {
+    var words = string.replace(/[.,]/g, '').split(/\s/);
+    var freqMap = {};
+    words.forEach(function(w) {
+        if (!freqMap[w] ) {
+            freqMap[w] = 0;
+        }
+        freqMap[w] += 1;
+    });
+
+    return freqMap;
+}
+
+function compare(a,b) {
+  if (a.size < b.size)
+    return 1;
+  if (a.size > b.size)
+    return -1;
+  return 0;
+}
+
+
+function returnWords(){
+  var data= document.getElementById('txt').value;
+  var arr = [];
+  var freq = wordFreq(data.toLowerCase());
+  Object.keys(freq).sort().forEach(function(word) {
+  		var obj = {};
+      obj["text"] = word;
+      obj["size"] = freq[word];
+      arr.push(obj);
+  });
+  return arr.filter((el) => {
+      return stopWords.indexOf(el.text) === -1;
+    }).sort(compare);
+};
+
+function drawWordCloud(){
+  document.getElementById("wordcloud").innerHTML = "";
+  var words = returnWords();
+  d3.wordcloud()
+    .size([350, 350])
+    .selector('#wordcloud')
+    .scale("log")
+    .fontWeight("bold")
+    .fill(d3.scale.ordinal().range(["#884400", "#448800", "#888800", "#444400"]))
+    .words(words)
+    .onwordclick(function(d, i) {
+      var div = document.getElementById('result');
+      div.innerHTML += d.text + ", ";
+    })
+    .start();
+  }
