091e42d5bac7e49753bbaf3f3adc6bf24e7cfe13
Tien Pham <r3dsky02@yahoo.com.vn>
2017-12-01 19:59:44 +0200

    Added correct seed data

--- app.js
+++ app.js
@@ -17,11 +17,65 @@ var url = process.env.MONGODB_URI ||"mongodb://localhost/appla";
 // Mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/appla");
 
 var insertDocument = function(db, callback) {
+
+  db.collection("messages").update(
+    {"message" : "Hi this is Tien Pham"},
+    {
+     "room_slug" : "aab",
+     "message" : "Hi this is Tien Pham",
+     "owner_info" : {
+         "first_name" : "Tien",
+         "last_name" : "Pham"
+     },
+     "created_at" : 121242
+   },
+   { upsert: true }
+  )
+
+  db.collection("messages").update(
+    {"message" : "and when you are having some kind of dialect all you are not speaking as a native of then it will be really strange if I'm"},
+    {
+     "room_slug" : "aab",
+     "message" : "and when you are having some kind of dialect all you are not speaking as a native of then it will be really strange if I'm",
+     "owner_info" : {
+         "first_name" : "Harri",
+         "last_name" : "Airaksinen"
+     },
+     "created_at" : 121238
+   },
+   { upsert: true }
+  )
+
+  db.collection("messages").update(
+    {"message" : "when there is no context when there is no surrounding words in the sentence tennis more difficult for the speech recognition application to have"},
+    {"room_slug" : "aab",
+       "message" : "when there is no context when there is no surrounding words in the sentence tennis more difficult for the speech recognition application to have",
+       "owner_info" : {
+           "first_name" : "Olli",
+           "last_name" : "Alm"
+       },
+       "created_at" : 121240
+   },
+   { upsert: true }
+  )
+
+  db.collection("messages").update(
+    {"message" : "alkaa varmaan vaikuttaa siihen tota katotaan mitä se tekee todennäköistä vaikuttaa myös sitten tänne minua asetelma ja mikrofonit ja Moi mitä tässä vielä ja sitten pitäisi tehdä silleen että et niinku nyt kohti tämmönen juttu Niin niin Minkälaisen"},
+    {"room_slug" : "aab",
+       "message" : "alkaa varmaan vaikuttaa siihen tota katotaan mitä se tekee todennäköistä vaikuttaa myös sitten tänne minua asetelma ja mikrofonit ja Moi mitä tässä vielä ja sitten pitäisi tehdä silleen että et niinku nyt kohti tämmönen juttu Niin niin Minkälaisen",
+       "owner_info" : {
+           "first_name" : "Olli",
+           "last_name" : "Alm"
+       },
+       "created_at" : 121222
+   },
+   { upsert: true }
+  )
+
 //    db.collection('messages').insertOne( {
 //     "room_slug" : "aab",
 //     "message" : "Hi this is Tien Pham",
 //     "owner_info" : {
-//         "owner_id" : ObjectId("5a20703a6800d66b6fa264e5"),
 //         "first_name" : "Tien",
 //         "last_name" : "Pham"
 //     },
@@ -33,7 +87,6 @@ var insertDocument = function(db, callback) {
 //    "room_slug" : "aab",
 //    "message" : "tyyni höpöhöpö Demon Katotaan jos se toimii oikein nyt tässä sitten tää on Googlen puhe puhe rajapinnan päälle rakennettu juttu et jos sulla on oma android-puhelin imuroida keskipiste sillä tavalla se tallentaa sun puhetta että tää laittaa mun puhetta liittyisin keskustelu analyysi homma ei toimi verrattain hyvin niinku näin koska suurinpiirtein",
 //    "owner_info" : {
-//        "owner_id" : ObjectId("5a20703a6800d66b6fa264e5"),
 //        "first_name" : "Olli",
 //        "last_name" : "Alm"
 //    },
@@ -44,13 +97,12 @@ var insertDocument = function(db, callback) {
 //    "room_slug" : "aab",
 //    "message" : "No niin kokeillaas Mä en tiedä toimiiko ja 10 Mä juttelin puheentunnistuksen Mitä ennen vanhaan ei ole ollenkaan mahdollista niin tää toimii äänellä kuin Äidillä oli sitten Tosiaan kun Johanna ja Väri vaihtelee puhelinta tässä ja se ihan sujuvasti jatkaa Eli se mallit ovat nykyään aika hienovaraisia ennen vanhan mallin tuli enemmän sellaisia treenattiin tietyille äänille ja sitten piti etukäteen kertoo että tämä puhua on tämän tyyppinen ihminen Mitähän se siellä puhuu väri vaihtelee puhelinta sujuvasti",
 //    "owner_info" : {
-//        "owner_id" : ObjectId("5a20703a6800d66b6fa264e3"),
-//        "first_name" : "Tien",
-//        "last_name" : "Pham"
+//        "first_name" : "Harri",
+//        "last_name" : "Airaksinen"
 //    },
 //    "created_at" : 121241
 //   });
-  db.collection('messages').drop();
+  // db.collection('messages').drop();
 };
 
 MongoClient.connect(url, function(err, db) {
